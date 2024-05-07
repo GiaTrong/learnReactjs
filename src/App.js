de8f9b78575540419f0c5d4a06265e340b4a5886
GiaTrong <hoanggiatrang01@gmail.com>
2024-05-07 14:57:48 +0700

    feat: Học về react Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponets';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+
+      <MyComponent></MyComponent>
     </div>
   );
 }
